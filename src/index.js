7b1501abb141640def029cb628040d40879c1d30
Dzhem Aptula <dzhem.aptula@gmail.com>
2021-12-09 20:03:37 +0100

    Initial commit of the nodejs application

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const Koa = require('koa');
+const Router = require('@koa/router');
+const config = require('config');
+const {
+    getLogger
+} = require('./core/logging')
+const bodyParser = require('koa-bodyparser');
+
+const NODE_ENV = config.get('env');
+const LOG_LEVEL = config.get('log.level');
+const LOG_DISABLED = config.get('log.disabled');
+
+const app = new Koa();
+const logger = getLogger();
+
+console.log(`log level ${LOG_LEVEL}, logs enabled: ${LOG_DISABLED !== true}`)
+
+app.use(bodyParser());
+
+app.use(async (ctx, next) => {
+    logger.info(JSON.stringify(ctx.request));
+    logger.info(JSON.stringify(ctx.request.body));
+    if (ctx.request.method === 'GET' && ctx.request.url === "/api/transactions") {
+        ctx.body = "[{'user': 'Benjamin', 'amount': 100, 'place': 'Irish Pub', date: '2021-08-15' }]";
+    } else {
+        ctx.body = 'Goodbye world';
+    }
+    next();
+});
+
+const router = new Router();
+
+logger.info(`🚀 Server listening on http://localhost:9000`);
+app.listen(9000);
